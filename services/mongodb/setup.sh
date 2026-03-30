ed6bc4c57202a74a0c3f76d0912c8bcdbdc18912
muysengly-itc <muy.sengly@itc.edu.kh>
2026-03-30 14:08:09 +0700

    update

--- services/mongodb/setup.sh
+++ services/mongodb/setup.sh
@@ -12,7 +12,6 @@ docker run -d \
     --name mongodb \
     --restart unless-stopped \
     --env-file .env \
-    -v /mnt/storage/mongodb/data:/data/db \
-    -v /mnt/storage/mongodb/var/log/mongodb:/var/log/mongodb \
     -p 27017:27017 \
     mongo:latest
+
